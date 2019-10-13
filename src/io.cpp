3f41e207c54dcf5154009b8e16da10c7953d105e
bruno <bvillasen@gmail.com>
2019-10-13 14:35:07 -0700

    separate load data domain pfft

--- src/io.cpp
+++ src/io.cpp
@@ -2584,7 +2584,7 @@ void Grid3D::Read_Grid_HDF5(hid_t file_id, struct parameters P)
       }
       
     }
-    #endif
+    #else
     
     // need a dataset buffer to remap fastest index
     dataset_buffer = (Real *) malloc(H.nz_real*H.ny_real*H.nx_real*sizeof(Real));
@@ -2997,6 +2997,9 @@ void Grid3D::Read_Grid_HDF5(hid_t file_id, struct parameters P)
     #endif//SCALAR
   }
   free(dataset_buffer);
+  
+  
+  #endif //CUSTOM_DOMAIN_PFFT
 
 }
 #endif
