6af151c99658d779c6c6ca536a11de637d13eb89
bruno <bvillasen@gmail.com>
2019-10-14 10:30:16 -0700

    print global domain

--- src/particles/io_particles.cpp
+++ src/particles/io_particles.cpp
@@ -277,15 +277,16 @@ void Particles_3D::Load_Particles_Data_HDF5(hid_t file_id, int nfile, struct par
   
     in_local = true;
     if ( pPos_x < G.domainMin_x || pPos_x > G.domainMax_x ){
-      std::cout << " Particle outside global domain " << std::endl;
+      std::cout << " Particle outside global domain: "<< pPos_x << " [ " << G.domainMin_x << " , " << G.domainMax_x << " ]" << std::endl;
+      
       // continue;
     }
     if ( pPos_y < G.domainMin_y || pPos_y > G.domainMax_y ){
-      std::cout << " Particle outside global domain " << std::endl;
+      std::cout << " Particle outside global domain: "<< pPos_y << " [ " << G.domainMin_y << " , " << G.domainMax_y << " ]" << std::endl;
       // continue;
     }
     if ( pPos_z < G.domainMin_z || pPos_z > G.domainMax_z ){
-      std::cout << " Particle outside global domain " << std::endl;
+      std::cout << " Particle outside global domain: "<< pPos_z << " [ " << G.domainMin_z << " , " << G.domainMax_z << " ]" << std::endl;
       // continue;
     }
     if ( pPos_x < G.xMin || pPos_x >= G.xMax ) in_local = false;
