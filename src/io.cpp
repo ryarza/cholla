a7cd5ebc7c59d1f518a5105e7047b35eefd58d28
ryarza <ryarza@ucsc.edu>
2020-06-21 09:40:10 -0700

    Subcells for initial conditions

--- src/io.cpp
+++ src/io.cpp
@@ -454,12 +454,12 @@ void Grid3D::Write_Header_HDF5(hid_t file_id)
   status = H5Aclose(attribute_id);
  
   #ifdef STARS
-  attribute_id = H5Acreate(file_id, "stellarMass", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
-  status = H5Awrite(attribute_id, H5T_NATIVE_DOUBLE, &Star.stellarMass);
+  attribute_id = H5Acreate(file_id, "Mstar", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
+  status = H5Awrite(attribute_id, H5T_NATIVE_DOUBLE, &Star.Mstar);
   status = H5Aclose(attribute_id);
   
-  attribute_id = H5Acreate(file_id, "stellarRadius", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
-  status = H5Awrite(attribute_id, H5T_NATIVE_DOUBLE, &Star.stellarRadius);
+  attribute_id = H5Acreate(file_id, "Rstar", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
+  status = H5Awrite(attribute_id, H5T_NATIVE_DOUBLE, &Star.Rstar);
   status = H5Aclose(attribute_id);
 
   attribute_id = H5Acreate(file_id, "polyN", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT);
